058d398006fb4f3cfdb4dc5a5ba12cc633c8a4c7
James Nguyen <janguy@microsoft.com>
2018-11-29 11:46:19 -0800

    Initial template commit

---
+++ notebooks/scana_hackathon.py
@@ -0,0 +1,81 @@
+# Databricks notebook source
+# MAGIC %md # Machine Learning Development Template
+
+# COMMAND ----------
+
+# MAGIC %md ## Setup data access and loading data
+
+# COMMAND ----------
+
+# MAGIC %md #### Mounting Blob to Azure Databricks 
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md #### Loading Data from SQL DW
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md #### Loading Data from Blob 
+
+# COMMAND ----------
+
+# MAGIC %md ## Exploratory Data Analysis
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md ## Data Cleaning and enrichment
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md ## Data Preperation for Machine Learning Model 
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md ## ML for Classification
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md ## ML for Regression
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md ## Timeseries forecast
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md ## Scoring and Persisting Result to SQL DW
+
+# COMMAND ----------
+
