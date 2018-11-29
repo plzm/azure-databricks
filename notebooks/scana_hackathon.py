c7dce6e0fb0af74f32596722f717f203f94b4bbf
James Nguyen <janguy@microsoft.com>
2018-11-29 14:22:34 -0800

    Adding weather API calling

--- notebooks/scana_hackathon.py
+++ notebooks/scana_hackathon.py
@@ -27,9 +27,19 @@
 
 # COMMAND ----------
 
+taxes2013 = spark.read.format("csv").option("header", "true").load("dbfs:/mnt/demo/csv/2013_soi_zipcode_agi.csv").drop("_c0")
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 # MAGIC %md ## Exploratory Data Analysis
 
 # COMMAND ----------
+
 
 
 
@@ -38,6 +48,35 @@
 # MAGIC %md ## Data Cleaning and enrichment
 
 # COMMAND ----------
+
+display(taxes2013.summary())
+from pyspark.sql import functions as F
+from pyspark.sql.functions import udf
+from pyspark.sql.types import IntegerType
+import json
+import requests
+import numpy as np
+import time
+APIKEY = "ff15bba527af804811d252aef420c02d"
+endpoint= "http://api.openweathermap.org/data/2.5/forecast?zip={0},us&APPID={1}"
+taxes2013_l = taxes2013.limit(20)
+def get_temp_forecast(zipcode):
+      response = requests.post(endpoint.format(zipcode,APIKEY))
+      #Dealing with threshold exceeding exception, retry util we can call the api
+      while response.status_code == 429:
+        time.sleep(1)
+        response = requests.post(endpoint.format(zipcode,APIKEY))
+
+      if response.status_code == 200:
+          return json.loads(response.content.decode("utf-8"))['list'][0]['main']['temp']
+
+      else:
+          return(response.status_code)
+#           raise Exception(str(response.status_code)+":" +response.text )
+  
+get_temp_forecast_udf = udf(get_temp_forecast)  
+
+display(taxes2013_l.withColumn("forecast_temp", get_temp_forecast_udf("zipcode")))
 
 
 
