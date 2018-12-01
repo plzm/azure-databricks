f061936b2427af00ad66eff0e1c90dc6287aa0d5
Patrick El-Azem <paelaz@microsoft.com>
2018-12-01 17:25:17 -0500

    Update: .gitignore ADBHack notebooks

--- ADBHack/adb_9_classification.py
+++ ADBHack/adb_9_classification.py
@@ -1,1 +1,41 @@
 # Databricks notebook source
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.feature import VectorAssembler
+
+# COMMAND ----------
+
+# MAGIC %run ./adb_3_ingest_to_df
+
+# COMMAND ----------
+
+df_flights_full_clean = df_flights_full\
+  .dropDuplicates()\
+  .na.fill(0, ["DepDelay", "ArrDelay"])
+
+df_flights_full_clean.count()
+
+# COMMAND ----------
+
+# Normalize
+
+
+
+# COMMAND ----------
+
+# Feature Engineering
+
+
+
+# COMMAND ----------
+
+# Get the numeric columns
+
+df_numerics = df_flights_full_clean.select("DayofMonth", "DayOfWeek", "OriginAirportID", "DestAirportID", "DepDelay", ((col("ArrDelay") > 15).cast("Int").alias("Late")))
+
+display(df_numerics)
+
+# COMMAND ----------
+
