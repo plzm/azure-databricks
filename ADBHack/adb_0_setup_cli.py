e0f56efe77dd7da11257254518a675f4319eb751
Patrick El-Azem <paelaz@microsoft.com>
2018-12-01 17:11:49 -0500

    Initial commit

---
+++ ADBHack/adb_0_setup_cli.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC Reference:<br>
+# MAGIC https://docs.azuredatabricks.net/user-guide/dev-tools/databricks-cli.html<br>
+# MAGIC https://docs.azuredatabricks.net/api/latest/authentication.html<br>
+# MAGIC 
+# MAGIC Note: Databricks CLI is experimental at this time. Seems to not work on Ubuntu 18.x yet. Seems to work on W10U shell.
+# MAGIC 
+# MAGIC Create a personal access token in Databricks user settings before setting up authentication.
+# MAGIC 
+# MAGIC Set up authentication: use a hostname like https://eastus.azuredatabricks.net. Do not append ?o= etc.
+
+# COMMAND ----------
+
