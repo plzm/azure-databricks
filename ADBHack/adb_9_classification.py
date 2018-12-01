851f20b27b65d68aebfbda329095aa9e01f88e74
Patrick El-Azem <paelaz@microsoft.com>
2018-12-01 17:39:15 -0500

    Update: .gitignore ADBHack notebooks

--- ADBHack/adb_9_classification.py
+++ ADBHack/adb_9_classification.py
@@ -39,3 +39,46 @@ display(df_numerics)
 
 # COMMAND ----------
 
+# Split the data into training and testing sets
+
+splits = df_numerics.randomSplit([0.7, 0.3])
+df_train = splits[0]
+df_test = splits[1]
+train_rows = str(df_train.count())
+test_rows = str(df_test.count())
+print("Training Rows:" + train_rows + " Testing Rows:" + test_rows)
+
+# COMMAND ----------
+
+# Assemble a feature vector, and designate the label column
+
+assembler = VectorAssembler(inputCols = ["DayofMonth", "DayOfWeek", "OriginAirportID", "DestAirportID", "DepDelay"], outputCol="features")
+
+training = assembler.transform(df_train).select(col("features"), col("Late").alias("label"))
+
+display(training)
+
+# COMMAND ----------
+
+# Train a model using logistic regression
+
+lr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10, regParam=0.3)
+model = lr.fit(training)
+
+# COMMAND ----------
+
+# Test model with testing data
+
+testing = assembler.transform(df_test).select(col("features"), col("Late").alias("realLabel"))
+display(testing)
+
+# COMMAND ----------
+
+# Test the model
+
+prediction = model.transform(testing)
+predicted = prediction.select("features", "probability", "prediction", "realLabel")
+display(predicted.take(1000))
+
+# COMMAND ----------
+
